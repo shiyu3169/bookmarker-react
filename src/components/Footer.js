e01ddebaf41c7ba299b05997d206ec26d21d5ec0
Shiyu Wang <shiyu3169@gmail.com>
2019-11-13 12:59:52 -0500

    finish

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="fixed-bottom text-center pb-2">
+      &copy; 2019 Bookmarker
+    </footer>
+  );
+}
+
+export default Footer;
